346cc899e8e03c23de9544b6e572516bed190460
Mani_Ananand <slmanikandan35@gmail.com>
2018-10-02 18:35:20 +0530

    footer 10% layout

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  selector     : 'app-footer',
+  templateUrl  : './footer.component.html',
+  styleUrls    : ['./footer.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class FooterComponent implements OnInit {
 
   constructor() { }
